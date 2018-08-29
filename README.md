381c3820821a3eac33b327a1fc4050d1277b355e
WooSung Choi <johnwschoi@gmail.com>
2018-08-29 02:22:43 -0700

    neighborhood map final

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# neighborhood-map
+# Neighborhood Map Project
+## Udacity Full Stack Web Developer Nanodegree
+Developed a single-page web application using jQuery, KnockoutJS, Bootstrap along with GoogleMaps API and Foursquare API.
+Google Map used for map feature and streatview if available, foursquare used to retrieve address from long and lat.
+KnockoutJS framework used for MVM).
+When minimized screen, can only navigate by markers from the map.
+## Frameworks, Libraries, and APIs
+HTML5, CSS, Javascript, KnockoutJS, jQuery, Google Map API, FourSquareAPI
+## Instruction
+1. Clone or download this repository.
+```
+git clone https://github.com/wchoi32/neighborhood-map.git
+```
+2. Navigate to the cloned/downloaded file and open index.html (no modules or external library required to download)
---
+++ client/js/locations.js
@@ -0,0 +1,86 @@
+var locations = [
+  {
+    title: 'Googleplex',
+    location: {
+      lat: 37.423199,
+      lng: -122.084068
+    }
+  },
+  {
+    title: 'Facebook HQ',
+    location: {
+      lat: 37.484379,
+      lng: -122.148308
+    }
+  },
+  {
+    title: 'Airbnb HQ',
+    location: {
+      lat: 37.771707,
+      lng: -122.405377
+    }
+  },
+  {
+    title: 'Uber HQ',
+    location: {
+      lat: 37.775316,
+      lng: -122.417437
+    }
+  },
+  {
+    title: 'LinkedIn HQ',
+    location: {
+      lat: 37.392752,
+      lng: -122.042149
+    }
+  },
+  {
+    title: 'Lyft HQ',
+    location: {
+      lat: 37.776597,
+      lng: -122.391949
+    }
+  },
+  {
+    title: 'Pintrest HQ',
+    location: {
+      lat: 37.775247,
+      lng: -122.399738
+    }
+  },
+  {
+    title: 'Salesforce HQ',
+    location: {
+      lat: 37.794006,
+      lng: -122.394962
+    }
+  },
+  {
+    title: 'Yelp HQ',
+    location: {
+      lat: 37.786662,
+      lng: -122.399972
+    }
+  },
+  {
+    title: 'Youtube HQ',
+    location: {
+      lat: 37.628158,
+      lng: -122.426455
+    }
+  },
+  {
+    title: 'Slack HQ',
+    location: {
+      lat: 37.788531,
+      lng: -122.396728
+    }
+  },
+  {
+    title: 'Ebay HQ',
+    location: {
+      lat: 37.295145,
+      lng: -121.926111
+    }
+  },
+];
